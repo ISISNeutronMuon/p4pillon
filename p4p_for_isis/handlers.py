b43be50d05e39b84c79d3546f8809b849f341721
Kathryn Baker <k.baker@stfc.ac.uk>
2024-05-07 16:33:08 +0100

    [ADD] examples for updating PVs either from server or with client

--- p4p_for_isis/handlers.py
+++ p4p_for_isis/handlers.py
@@ -7,8 +7,18 @@ from p4p.server.thread import Handler, SharedPV
 from .utils import time_in_seconds_and_nanoseconds
 
 
+class ROHandler(Handler):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def put(self, pv_: SharedPV, op: ServerOperation):
+        """This callback is run whenever you do `ctx.put` on the PV"""
+        print(f"{op.name()} is read-only from a client perspective. Values can only be changed from within the server.")
+        op.done()
+
+
 class RWHandler(Handler):
-    """A handler that allows two methods of changing the value of a PV"""
+    """Example handler for processing updates to PVs"""
 
     def __init__(self) -> None:
         super().__init__()
@@ -19,9 +29,17 @@ class RWHandler(Handler):
             value.timeStamp = {"secondsPastEpoch": seconds, "nanoseconds": nanoseconds}
         return value
 
+    def process_update(self, value: Value) -> Value:
+        value = self._update_timestamp(value)
+        return value
+
     def put(self, pv_: SharedPV, op: ServerOperation):
         """This callback is run whenever you do `ctx.put` on the PV"""
-        print("RW handler")
-        value = self._update_timestamp(op.value())
-        pv_.post(value)
+        # NOTE op.peer() and op.account() can be used to get access to IP address
+        # and account owner of the client request
+        pv_, op = self.on_put(pv_, op)
+        pv_.post(op.value())
         op.done()
+
+    def on_put(self, pv_: SharedPV, op: ServerOperation):
+        return pv_, op
