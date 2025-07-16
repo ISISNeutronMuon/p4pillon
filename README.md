fe8f290a15a73cdfd653bf382cefaf3f8c574829
Ivan Finch <ivan.finch@stfc.ac.uk>
2025-07-16 17:38:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # p4p_ext
-Extensions and additional functionality for the p4p library
+Opinionated extensions and additional functionality for the p4p library.
+
+### Python Version
+These extensions make extensive use of typing. As such they require Python 3.9 or later.
+
+### p4p Version
+These extensions require a version of p4p with [PR172](https://github.com/epics-base/p4p/pull/172). This will be installed as a dependency with the package, but note there is a potential for conflict with other installed instances.
+
+## Extensions
+### CompositeHandler
+The base version of p4p only allows a single Handler class to be associated with a SharedPV. This makes it complex to combine multiple handlers from different sources. It also means that if a Handler is used to handle NormativeType logic then it either precludes a user handler or requires subclassing.
+
+The supplied CompositeHandler has the same interface as the base Handler. It uses an OrderedDict to store componenent Handlers (standard Handlers, per base p4p) and calls them in the specified order.
+
+It is designed to work with the Handler class, and is **not** designed to work with the Handler decorators.
---
+++ p4p_ext/handler.py
@@ -0,0 +1,103 @@
+"""
+Composite Handler allows multiple standard handlers to be combined into a single handler.
+
+And ordered dictionary is used to make the component handlers accessible by name.
+The ordered dictionary also controls the order in which the handlers are called.
+"""
+
+from collections import OrderedDict
+from typing import Optional
+
+from p4p import Value
+from p4p.server import ServerOperation
+from p4p.server.thread import Handler, SharedPV
+from typing_extensions import deprecated
+
+
+class HandlerException(Exception):
+    """Exception raised for errors in the handler operations."""
+
+
+class AbortHandlerException(HandlerException):
+    """Exception raised to abort the current operation in the handler."""
+
+    def __init__(self, message: str = "Operation aborted"):
+        super().__init__(message)
+        self.message = message
+
+
+class CompositeHandler(Handler):
+    """Composite Handler for combining multiple component handlers into a single handler."""
+
+    def __init__(self, handlers: Optional[OrderedDict[str, Handler]] = None):
+        """Initialize the CompositeHandler with an optional list of handlers."""
+        super().__init__()
+        self.handlers = handlers
+
+    def __getitem__(self, name: str) -> Handler | None:
+        if self.handlers:
+            return self.handlers[name]
+
+        return None
+
+    def open(self, value: Value):
+        """Open all handlers in the composite handler."""
+        if self.handlers:
+            for handler in self.handlers.values():
+                handler.open(value)
+
+    def put(self, pv: SharedPV, op: ServerOperation):
+        errmsg = None
+
+        if self.handlers:
+            for handler in self.handlers.values():
+                try:
+                    handler.put(pv, op)
+                except AbortHandlerException as e:
+                    errmsg = e.message
+                    break
+
+        op.done(error=errmsg)
+
+    def post(self, pv: SharedPV, value: Value):
+        if self.handlers:
+            for handler in self.handlers.values():
+                handler.post(pv, value)
+
+    def rpc(self, pv: SharedPV, op: ServerOperation):
+        errmsg = None
+
+        if self.handlers:
+            for handler in self.handlers.values():
+                try:
+                    handler.rpc(pv, op)
+                except AbortHandlerException as e:
+                    errmsg = e.message
+                    break
+
+        op.done(error=errmsg)
+
+    def on_first_connect(self, pv: SharedPV):
+        """Called when the first client connects to the PV."""
+        if self.handlers:
+            for handler in self.handlers.values():
+                handler.onFirstConnect(pv)
+
+    @deprecated("onFirstConnect is deprecated, use on_first_connect instead")
+    def onFirstConnect(self, pv: Value):
+        self.on_first_connect(pv)
+
+    def on_last_connect(self, pv: SharedPV):
+        """Called when the last client channel is closed."""
+        if self.handlers:
+            for handler in self.handlers.values():
+                handler.onFirstConnect(pv)
+
+    @deprecated("onLastDisconnect is deprecated, use on_last_connect instead")
+    def onLastDisconnect(self, pv: Value):
+        self.on_last_connect(pv)
+
+    def close(self, pv: SharedPV):
+        if self.handlers:
+            for handler in self.handlers.values():
+                handler.close(pv)
