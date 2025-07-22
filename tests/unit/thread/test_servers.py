fed9301eaabc1e3cfa15186795256566a024a579
Ivan Finch <ivan.finch@stfc.ac.uk>
2025-07-22 15:43:11 +0100

    Add server back in as a purely threaded version

---
+++ tests/unit/thread/test_servers.py
@@ -0,0 +1,145 @@
+import logging
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+from p4p.server import StaticProvider
+
+from p4p_ext.thread.server import SimpleServer
+
+root_dir = Path(__file__).parents[2]
+
+
+def test_server_instantiation():
+    server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+    assert server.ioc_name == "TESTIOC"
+    assert server.section == "controls testing"
+    assert server.description == "server for unit tests"
+    assert server.prefix == "DEV:"
+
+    # before we explicitly call `start()`, the server shouldn't exist
+    assert server._server is None
+    assert isinstance(server._provider, StaticProvider)
+    # and we also shouldn't have any PVs configured
+    assert server.pvlist == []
+
+
+@pytest.mark.parametrize(
+    "pv_name",
+    [("TEST:PV"), ("DEV:TEST:PV")],
+)
+def test_server_retrieve_pvs(mock_recipe, pv_name):
+    server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+    server.add_pv(pv_name, mock_recipe)
+
+    # we should be able to access the PV either with the full prefix added or without it
+    assert server["TEST:PV"] == mock_recipe.create_pv.return_value
+    assert server["DEV:TEST:PV"] == mock_recipe.create_pv.return_value
+
+
+@pytest.mark.xfail(reason="Unsure why the mock provider is failing at server.start()")
+@patch("p4p_ext.thread.server.StaticProvider", autospec=True)
+@patch("p4p_ext.thread.server.SimpleServer", autospec=True)
+def test_server_start(server, provider, caplog, mock_isispv):
+    test_server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+
+    mock_isispv.on_start_methods = []
+    test_server._pvs = {"DEV:TEST:PV:1": mock_isispv}
+    print(len(mock_isispv.on_start_methods))
+
+    assert test_server._running is False
+    with caplog.at_level(logging.DEBUG):
+        test_server.start()
+    assert len(caplog.records) == 1
+    provider.return_value.add.assert_called_once_with("DEV:TEST:PV:1", mock_isispv)
+    server.assert_called_once_with(providers=[provider.return_value])
+    assert test_server._running is True
+
+
+@pytest.mark.xfail(reason="Unsure why the mock provider is failing at server.start()")
+@patch("p4p_ext.thread.server.StaticProvider", autospec=True)
+@patch("p4p_ext.thread.server.SimpleServer", autospec=True)
+@patch("p4p_ext.pvrecipe.PVScalarRecipe", autospec=True)
+def test_server_add_pv(recipe, server, provider, caplog):
+    test_server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+
+    test_server.start()
+
+    with caplog.at_level(logging.DEBUG):
+        new_name = "TEST:PV:2"
+        test_server.add_pv(new_name, recipe.return_value)
+    assert test_server["TEST:PV:2"] is recipe.return_value.create_pv.return_value
+    provider.return_value.add.assert_called_once_with("DEV:TEST:PV:2", recipe.return_value.create_pv.return_value)
+
+    assert len(caplog.messages) == 1
+    assert caplog.messages[0] == "Added DEV:TEST:PV:2 to server"
+
+
+@patch("p4p_ext.thread.server.StaticProvider", autospec=True)
+@patch("p4p_ext.thread.server.SimpleServer", autospec=True)
+def test_server_stop(server, provider, caplog, mock_isispv):
+    test_server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+
+    test_server._running = True
+    test_server._server = server.return_value
+    test_server._pvs = {"DEV:TEST:PV:1": mock_isispv}
+
+    with caplog.at_level(logging.DEBUG):
+        test_server.stop()
+
+    mock_isispv.close.assert_called_once_with()
+    provider.return_value.remove.assert_called_once_with("DEV:TEST:PV:1")
+    server.return_value.stop.assert_called_once_with()
+    assert test_server._running is False
+
+
+@patch("p4p_ext.thread.server.StaticProvider", autospec=True)
+@patch("p4p_ext.thread.server.SimpleServer", autospec=True)
+@patch("p4p_ext.pvrecipe.PVScalarRecipe", autospec=True)
+def test_server_remove_pv(recipe, server, provider, caplog, mock_isispv):
+    test_server = SimpleServer(
+        ioc_name="TESTIOC",
+        section="controls testing",
+        description="server for unit tests",
+        prefix="DEV:",
+    )
+
+    test_server._pvs["DEV:TEST:PV:1"] = mock_isispv
+    test_server._running = True
+    test_server._server = server.return_value
+
+    with caplog.at_level(logging.DEBUG):
+        test_server.remove_pv("TEST:PV:1")
+
+    mock_isispv.close.assert_called_once_with()
+    provider.return_value.remove.assert_called_once_with("DEV:TEST:PV:1")
+
+    assert test_server._pvs.get("DEV:TEST:PV:1") is None
+
+    assert len(caplog.messages) == 1
+    assert caplog.messages[0] == "Removed DEV:TEST:PV:1 from server"
