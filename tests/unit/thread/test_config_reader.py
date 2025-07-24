4f42d4670a332bd0707acbf0a544e364757d50b9
Kathryn Baker <k.baker@stfc.ac.uk>
2025-07-24 16:58:43 +0100

    additional test for Enums with config reader

--- tests/unit/thread/test_config_reader.py
+++ tests/unit/thread/test_config_reader.py
@@ -4,8 +4,8 @@ import numpy as np
 import pytest
 
 from p4p_ext.definitions import PVTypes
-from p4p_ext.pvrecipe import PVScalarRecipe
 from p4p_ext.thread.config_reader import parse_config
+from p4p_ext.thread.pvrecipe import PVEnumRecipe, PVScalarArrayRecipe, PVScalarRecipe
 
 
 @pytest.mark.parametrize(
@@ -28,13 +28,14 @@ def test_parse_config_with_server(name, config, pvtype):
     recipes = parse_config({name: config}, server)
     assert len(recipes) == 1
 
-    assert isinstance(recipes[name], PVScalarRecipe)
     assert recipes[name].description == config["description"]
     assert recipes[name].pvtype == pvtype
 
     if config.get("array_size"):
+        assert isinstance(recipes[name], PVScalarArrayRecipe)
         assert np.allclose(recipes[name].initial_value, np.array([0, 0, 0]))
     else:
+        assert isinstance(recipes[name], PVScalarRecipe)
         assert recipes[name].initial_value == config.get("initial", 0.0)
 
     assert server.add_pv.call_args_list[0][0][0] == name
@@ -51,3 +52,27 @@ def test_parse_config_syntax_errors(config, error_msg):
     with pytest.raises(SyntaxError) as e:
         parse_config({"TEST:PV": config})
     assert error_msg in str(e)
+
+
+@pytest.mark.parametrize(
+    "name, config, pvtype",
+    [
+        ("TEST:PV:ENUM", {"initial": {"index": 0, "choices": ["False", "True"]}, "type": "ENUM"}, PVTypes.ENUM),
+        ("TEST:PV:ENUM", {"initial": {"index": 1, "choices": ["OFF", "ON"]}, "type": "ENUM"}, PVTypes.ENUM),
+    ],
+)
+def test_parse_config_with_server_enum(name, config, pvtype):
+    server = MagicMock()
+
+    config["description"] = "test"
+
+    recipes = parse_config({name: config}, server)
+    assert len(recipes) == 1
+
+    assert isinstance(recipes[name], PVEnumRecipe)
+    assert recipes[name].description == config["description"]
+    assert recipes[name].pvtype == pvtype
+
+    assert server.add_pv.call_args_list[0][0][1].initial_value == config["initial"]
+    assert isinstance(server.add_pv.call_args_list[0][0][1], PVEnumRecipe)
+    assert server.add_pv.call_args_list[0][0][0] == name
