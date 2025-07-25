6dc2f05563a160627ff4de6e655ee77405f18617
Ivan Finch <ivan.finch@stfc.ac.uk>
2025-07-26 00:02:27 +0100

    Minor fixes to unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -28,9 +28,9 @@ def mock_pv():
 
 
 @pytest.fixture
-def mock_isispv():
-    with patch("p4p.server.thread.SharedPV", autospec=True) as shared_pv:
-        yield shared_pv
+def mock_sharednt():
+    with patch("p4p_ext.thread.sharednt.SharedNT", autospec=True) as shared_nt:
+        yield shared_nt
 
 
 @pytest.fixture
