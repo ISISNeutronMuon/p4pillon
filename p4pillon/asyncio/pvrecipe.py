526ad83e60b11b3360fdba8420161a59ef07ceb9
Ivan Finch <ivan.finch@stfc.ac.uk>
2025-09-21 22:01:11 +0100

    Add shared concurrency integration test

--- p4pillon/asyncio/pvrecipe.py
+++ p4pillon/asyncio/pvrecipe.py
@@ -1,7 +1,7 @@
 from typing import TypeVar
 
 from p4pillon.pvrecipe import *  # noqa: F403
-from p4pillon.server.thread import SharedPV as _SharedPV
+from p4pillon.server.asyncio import SharedPV as _SharedPV
 
 SharedPV = _SharedPV
 
