e79c19b37e0d908ef7e744beacc13af0c9e013f0
Ivan Finch <ivan.finch@stfc.ac.uk>
2026-07-14 18:58:50 +0100

    Add LOG to ruff options

--- p4pillon/rules/calc_rule.py
+++ p4pillon/rules/calc_rule.py
@@ -110,12 +110,12 @@ class CalcRule(BaseScalarRule):
             try:
                 val = self._server.get_pv_value(pv_name)
                 if val is None:
-                    logging.error("Failed to get pv %s", pv_name)
+                    logger.error("Failed to get pv %s", pv_name)
                     return None
                 pvs.append(val)
             except Exception:  # noqa: PERF203 -- per-item error handling around I/O, returns on first failure
                 # If there's an error getting the value of a pv return None
-                logging.exception("Failed to get pv %s", pv_name)
+                logger.exception("Failed to get pv %s", pv_name)
                 return None
 
         return pvs
