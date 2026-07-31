695ca2a4b57a93ae15bee2bfca0c303e5f0a095e
Ivan Finch <ivan.finch@stfc.ac.uk>
2026-07-31 13:03:09 +0100

    Fix with ruff --fix --unsafe-fixes

--- p4pillon/composite_handler.py
+++ p4pillon/composite_handler.py
@@ -38,7 +38,7 @@ class CompositeHandler(Handler, OrderedDict):
 
     def open(self, value: Value):
         """Open all handlers in the composite handler."""
-        for _name, handler in self.items():
+        for handler in self.values():
             handler.open(value)
 
     def put(self, pv: SharedPV, op: ServerOperation):
@@ -49,7 +49,7 @@ class CompositeHandler(Handler, OrderedDict):
 
         errmsg = None
 
-        for _name, handler in self.items():
+        for handler in self.values():
             try:
                 handler.put(pv, op)
             except AbortHandlerException as e:
@@ -63,7 +63,7 @@ class CompositeHandler(Handler, OrderedDict):
             op.done(error=errmsg)
 
     def post(self, pv: SharedPV, value: Value):
-        for _name, handler in self.items():
+        for handler in self.values():
             handler.post(pv, value)
 
     def rpc(self, pv: SharedPV, op: ServerOperation):
--- p4pillon/sharednt.py
+++ p4pillon/sharednt.py
@@ -38,7 +38,7 @@ def is_type_subset(fullset: Type, subset: Type) -> bool:
     """Check if the subset is a part of the fullset."""
 
     # For now we only support looking one level deep
-    test = all(x in fullset.keys() for x in subset.keys())
+    test = all(x in fullset for x in subset)
     if test:
         pass
     else:
