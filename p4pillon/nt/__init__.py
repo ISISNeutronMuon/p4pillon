54d4321846b4b433e801fce2341e63a9275da525
Ivan Finch <ivan.finch@stfc.ac.uk>
2026-07-31 12:52:41 +0100

    New ruff version adds new rules, these are the safe fixes

--- p4pillon/nt/__init__.py
+++ p4pillon/nt/__init__.py
@@ -2,7 +2,7 @@
 Monkey patch in required changes to NTEnum
 """
 
-from p4p.nt import *  # type: ignore # noqa: F403
+from p4p.nt import *  # type: ignore
 from p4p.nt import NTBase, NTEnum  # noqa: F401
 
 from p4pillon.nt.enum import NTEnum as _NTEnum
--- p4pillon/server/asyncio.py
+++ p4pillon/server/asyncio.py
@@ -13,12 +13,12 @@ from p4pillon.server.raw import SharedPV as _SharedPV
 p4p.server.raw.SharedPV = _SharedPV
 
 # pylint: disable=unused-import, wrong-import-order, wrong-import-position
-from p4p.server.thread import Handler, SharedPV  # noqa: E402, F401,
+from p4p.server.thread import Handler, SharedPV  # noqa: F401,
 
 #####
 # Monkey patching the Handler is a simpler operation as it's a straight
 # substitution with our new version.
 # pylint: disable=ungrouped-imports
-from p4pillon.server.raw import Handler as _Handler  # noqa: E402
+from p4pillon.server.raw import Handler as _Handler
 
 Handler = _Handler  # noqa: F811
